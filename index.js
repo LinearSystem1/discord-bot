fac4cb77217ffb27ecbef8028c75ac2bb2e2d14a
Linear <148605676+LinearSystem1@users.noreply.github.com>
2023-10-21 19:05:17 +0000

    installed

--- index.js
+++
@@ -1,5 +0,0 @@
-var hello = 0
-console.log(hello);
-console.log("meow");
-
-//HELPP HELP ME
---
+++ src/index.js
@@ -0,0 +1,13 @@
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [ '7:22']
+
+
+})
+
+var hello = 0
+console.log(hello);
+console.log("meow");
+
+//HELPP HELP ME
