03b88f48e7af9bb49606bb308af6dbf18362789c
Linear <148605676+LinearSystem1@users.noreply.github.com>
2023-10-23 02:02:43 +0000

    ees

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const { Client, IntentsBitField } = require('discord.js');
 
 const client = new Client({
-    intents: [ '7:22']
+    intents: [ 
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
 
+client.login("MTE2NTM1ODc4NzA4ODA5NzQ5MA.GSel28.B-txUQWykmDyp45Wv2yE2KLGn4NZPNZV38YBd4")
 
-})
 
 var hello = 0
 console.log(hello);
