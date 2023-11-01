5bb09629f28203998cfcb94d89f556b8c22bd43f
Linear <148605676+LinearSystem1@users.noreply.github.com>
2023-11-01 18:44:22 +0000

    ssdf

--- src/register-commands.js
+++ src/register-commands.js
@@ -51,7 +51,8 @@ const rest = new REST ({version: '10'}).setToken(process.env.TOKEN);
         console.log(`Started slash commands...`);
 
         await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            //Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            Routes.applicationCommands(process.env.CLIENT_ID),
             { body: commands }
         )
 
