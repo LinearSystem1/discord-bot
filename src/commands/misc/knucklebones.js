a17e5ebeeeca3a9c19406b861b54d5563aee9476
Linear <148605676+LinearSystem1@users.noreply.github.com>
2024-02-14 04:38:50 +0000

    uhgggggggg

--- src/commands/misc/knucklebones.js
+++ src/commands/misc/knucklebones.js
@@ -12,16 +12,28 @@ module.exports = {
         //I cant wait to write so much logic
         //const p1 = uhh
         //const p2 =...
-        const diceNum = [0, 1, 2, 3, 4, 5, 6]
-        var row1 = []
-        var row2 = []
-        var row3 = []
+        const diceNum = [1, 2, 3, 4, 5, 6]
+        const spaceState = [0, 1, 2, 3, 4, 5, 6]
+        var row1P1 = []
+        var row2P1 = []
+        var row3P1 = []
+        var row1P2 = []
+        var row2P2 = []
+        var row3P2 = []
 
         function declareDiceNum() {
             var dice = Math.floor(Math.random() * diceNum.length);
             return dice
         }
-        var dice = declareDiceNum()
-        interatction.editReply(dice)
+        var dice = declareDiceNum();
+        function appendDice() {
+            row1P1.push(diceNum[dice])
+            row2P1.push(diceNum[dice])
+            row3P1.push(diceNum[dice])
+            row1P2.push(diceNum[dice])
+            row2P2.push(diceNum[dice])
+            row3P2.push(diceNum[dice])
+        }
+        interatction.editReply("number" + dice)
     },
 };
